07721be20ff2c46fa2053b7dbea28a335e962830
jayde <jayde@JAYDENLAPTOP77>
2024-02-27 13:48:08 -0500

    Add project files.

---
+++ CashRegister/Form1.Designer.cs
@@ -0,0 +1,340 @@
+﻿namespace CashRegister
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.shopLabel = new System.Windows.Forms.Label();
+            this.reciptLabel = new System.Windows.Forms.Label();
+            this.rugbyBallCount = new System.Windows.Forms.Label();
+            this.rugbyBallInput = new System.Windows.Forms.TextBox();
+            this.rugbyCleatsLabel = new System.Windows.Forms.Label();
+            this.rugbyCleatsInput = new System.Windows.Forms.TextBox();
+            this.rugbyJerseyLabel = new System.Windows.Forms.Label();
+            this.rugbyJerseyInput = new System.Windows.Forms.TextBox();
+            this.calculateButton = new System.Windows.Forms.Button();
+            this.subtotalLabel = new System.Windows.Forms.Label();
+            this.subtotalOutput = new System.Windows.Forms.Label();
+            this.taxLabel = new System.Windows.Forms.Label();
+            this.taxOutput = new System.Windows.Forms.Label();
+            this.totalLabel = new System.Windows.Forms.Label();
+            this.totalOutput = new System.Windows.Forms.Label();
+            this.tenderedLabel = new System.Windows.Forms.Label();
+            this.tenderedInput = new System.Windows.Forms.TextBox();
+            this.changeButton = new System.Windows.Forms.Button();
+            this.changeLabel = new System.Windows.Forms.Label();
+            this.changeOutput = new System.Windows.Forms.Label();
+            this.printButton = new System.Windows.Forms.Button();
+            this.reciptNameLabel = new System.Windows.Forms.Label();
+            this.newOrderButton = new System.Windows.Forms.Button();
+            this.pricesButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // shopLabel
+            // 
+            this.shopLabel.BackColor = System.Drawing.Color.Firebrick;
+            this.shopLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.shopLabel.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.shopLabel.Location = new System.Drawing.Point(0, -4);
+            this.shopLabel.Name = "shopLabel";
+            this.shopLabel.Size = new System.Drawing.Size(646, 52);
+            this.shopLabel.TabIndex = 0;
+            this.shopLabel.Text = "Rugby Shop";
+            this.shopLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // reciptLabel
+            // 
+            this.reciptLabel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.reciptLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reciptLabel.Location = new System.Drawing.Point(328, 61);
+            this.reciptLabel.Name = "reciptLabel";
+            this.reciptLabel.Size = new System.Drawing.Size(299, 458);
+            this.reciptLabel.TabIndex = 1;
+            // 
+            // rugbyBallCount
+            // 
+            this.rugbyBallCount.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rugbyBallCount.Location = new System.Drawing.Point(1, 74);
+            this.rugbyBallCount.Name = "rugbyBallCount";
+            this.rugbyBallCount.Size = new System.Drawing.Size(211, 28);
+            this.rugbyBallCount.TabIndex = 2;
+            this.rugbyBallCount.Text = "# of Rugby Balls";
+            this.rugbyBallCount.Click += new System.EventHandler(this.rugbyBallCount_Click);
+            // 
+            // rugbyBallInput
+            // 
+            this.rugbyBallInput.Location = new System.Drawing.Point(211, 79);
+            this.rugbyBallInput.Name = "rugbyBallInput";
+            this.rugbyBallInput.Size = new System.Drawing.Size(100, 22);
+            this.rugbyBallInput.TabIndex = 3;
+            // 
+            // rugbyCleatsLabel
+            // 
+            this.rugbyCleatsLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rugbyCleatsLabel.Location = new System.Drawing.Point(1, 121);
+            this.rugbyCleatsLabel.Name = "rugbyCleatsLabel";
+            this.rugbyCleatsLabel.Size = new System.Drawing.Size(203, 28);
+            this.rugbyCleatsLabel.TabIndex = 4;
+            this.rugbyCleatsLabel.Text = "# of Pairs of Cleats";
+            // 
+            // rugbyCleatsInput
+            // 
+            this.rugbyCleatsInput.Location = new System.Drawing.Point(211, 125);
+            this.rugbyCleatsInput.Name = "rugbyCleatsInput";
+            this.rugbyCleatsInput.Size = new System.Drawing.Size(100, 22);
+            this.rugbyCleatsInput.TabIndex = 5;
+            // 
+            // rugbyJerseyLabel
+            // 
+            this.rugbyJerseyLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rugbyJerseyLabel.Location = new System.Drawing.Point(1, 163);
+            this.rugbyJerseyLabel.Name = "rugbyJerseyLabel";
+            this.rugbyJerseyLabel.Size = new System.Drawing.Size(203, 28);
+            this.rugbyJerseyLabel.TabIndex = 6;
+            this.rugbyJerseyLabel.Text = "# of Jerseys";
+            // 
+            // rugbyJerseyInput
+            // 
+            this.rugbyJerseyInput.Location = new System.Drawing.Point(211, 167);
+            this.rugbyJerseyInput.Name = "rugbyJerseyInput";
+            this.rugbyJerseyInput.Size = new System.Drawing.Size(100, 22);
+            this.rugbyJerseyInput.TabIndex = 7;
+            // 
+            // calculateButton
+            // 
+            this.calculateButton.Location = new System.Drawing.Point(67, 210);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(157, 48);
+            this.calculateButton.TabIndex = 8;
+            this.calculateButton.Text = "Calculate Total";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            // 
+            // subtotalLabel
+            // 
+            this.subtotalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalLabel.Location = new System.Drawing.Point(1, 273);
+            this.subtotalLabel.Name = "subtotalLabel";
+            this.subtotalLabel.Size = new System.Drawing.Size(126, 28);
+            this.subtotalLabel.TabIndex = 9;
+            this.subtotalLabel.Text = "Subtotal";
+            // 
+            // subtotalOutput
+            // 
+            this.subtotalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subtotalOutput.Location = new System.Drawing.Point(105, 273);
+            this.subtotalOutput.Name = "subtotalOutput";
+            this.subtotalOutput.Size = new System.Drawing.Size(217, 28);
+            this.subtotalOutput.TabIndex = 10;
+            this.subtotalOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // taxLabel
+            // 
+            this.taxLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxLabel.Location = new System.Drawing.Point(1, 301);
+            this.taxLabel.Name = "taxLabel";
+            this.taxLabel.Size = new System.Drawing.Size(126, 28);
+            this.taxLabel.TabIndex = 11;
+            this.taxLabel.Text = "Tax";
+            // 
+            // taxOutput
+            // 
+            this.taxOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.taxOutput.Location = new System.Drawing.Point(110, 301);
+            this.taxOutput.Name = "taxOutput";
+            this.taxOutput.Size = new System.Drawing.Size(212, 28);
+            this.taxOutput.TabIndex = 12;
+            this.taxOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // totalLabel
+            // 
+            this.totalLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalLabel.Location = new System.Drawing.Point(1, 329);
+            this.totalLabel.Name = "totalLabel";
+            this.totalLabel.Size = new System.Drawing.Size(126, 28);
+            this.totalLabel.TabIndex = 13;
+            this.totalLabel.Text = "Total";
+            // 
+            // totalOutput
+            // 
+            this.totalOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.totalOutput.Location = new System.Drawing.Point(100, 329);
+            this.totalOutput.Name = "totalOutput";
+            this.totalOutput.Size = new System.Drawing.Size(222, 28);
+            this.totalOutput.TabIndex = 14;
+            this.totalOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // tenderedLabel
+            // 
+            this.tenderedLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tenderedLabel.Location = new System.Drawing.Point(1, 380);
+            this.tenderedLabel.Name = "tenderedLabel";
+            this.tenderedLabel.Size = new System.Drawing.Size(126, 28);
+            this.tenderedLabel.TabIndex = 15;
+            this.tenderedLabel.Text = "Tendered";
+            // 
+            // tenderedInput
+            // 
+            this.tenderedInput.Location = new System.Drawing.Point(211, 386);
+            this.tenderedInput.Name = "tenderedInput";
+            this.tenderedInput.Size = new System.Drawing.Size(100, 22);
+            this.tenderedInput.TabIndex = 16;
+            // 
+            // changeButton
+            // 
+            this.changeButton.Enabled = false;
+            this.changeButton.Location = new System.Drawing.Point(67, 428);
+            this.changeButton.Name = "changeButton";
+            this.changeButton.Size = new System.Drawing.Size(157, 48);
+            this.changeButton.TabIndex = 17;
+            this.changeButton.Text = "Calculate Change";
+            this.changeButton.UseVisualStyleBackColor = true;
+            this.changeButton.Click += new System.EventHandler(this.changeButton_Click);
+            // 
+            // changeLabel
+            // 
+            this.changeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeLabel.Location = new System.Drawing.Point(1, 491);
+            this.changeLabel.Name = "changeLabel";
+            this.changeLabel.Size = new System.Drawing.Size(126, 28);
+            this.changeLabel.TabIndex = 18;
+            this.changeLabel.Text = "Change";
+            // 
+            // changeOutput
+            // 
+            this.changeOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.changeOutput.Location = new System.Drawing.Point(95, 491);
+            this.changeOutput.Name = "changeOutput";
+            this.changeOutput.Size = new System.Drawing.Size(227, 28);
+            this.changeOutput.TabIndex = 19;
+            this.changeOutput.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // printButton
+            // 
+            this.printButton.Enabled = false;
+            this.printButton.Location = new System.Drawing.Point(67, 535);
+            this.printButton.Name = "printButton";
+            this.printButton.Size = new System.Drawing.Size(157, 48);
+            this.printButton.TabIndex = 20;
+            this.printButton.Text = "Print Recipt";
+            this.printButton.UseVisualStyleBackColor = true;
+            this.printButton.Click += new System.EventHandler(this.printButton_Click);
+            // 
+            // reciptNameLabel
+            // 
+            this.reciptNameLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reciptNameLabel.Location = new System.Drawing.Point(386, 75);
+            this.reciptNameLabel.Name = "reciptNameLabel";
+            this.reciptNameLabel.Size = new System.Drawing.Size(188, 28);
+            this.reciptNameLabel.TabIndex = 21;
+            this.reciptNameLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // newOrderButton
+            // 
+            this.newOrderButton.Location = new System.Drawing.Point(498, 535);
+            this.newOrderButton.Name = "newOrderButton";
+            this.newOrderButton.Size = new System.Drawing.Size(129, 48);
+            this.newOrderButton.TabIndex = 22;
+            this.newOrderButton.Text = "New Order";
+            this.newOrderButton.UseVisualStyleBackColor = true;
+            this.newOrderButton.Click += new System.EventHandler(this.newOrderButton_Click);
+            // 
+            // pricesButton
+            // 
+            this.pricesButton.Location = new System.Drawing.Point(328, 535);
+            this.pricesButton.Name = "pricesButton";
+            this.pricesButton.Size = new System.Drawing.Size(136, 48);
+            this.pricesButton.TabIndex = 23;
+            this.pricesButton.Text = "Prices";
+            this.pricesButton.UseVisualStyleBackColor = true;
+            this.pricesButton.Click += new System.EventHandler(this.pricesButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 595);
+            this.Controls.Add(this.pricesButton);
+            this.Controls.Add(this.newOrderButton);
+            this.Controls.Add(this.reciptNameLabel);
+            this.Controls.Add(this.printButton);
+            this.Controls.Add(this.changeOutput);
+            this.Controls.Add(this.changeLabel);
+            this.Controls.Add(this.changeButton);
+            this.Controls.Add(this.tenderedInput);
+            this.Controls.Add(this.tenderedLabel);
+            this.Controls.Add(this.totalOutput);
+            this.Controls.Add(this.totalLabel);
+            this.Controls.Add(this.taxOutput);
+            this.Controls.Add(this.taxLabel);
+            this.Controls.Add(this.subtotalOutput);
+            this.Controls.Add(this.subtotalLabel);
+            this.Controls.Add(this.calculateButton);
+            this.Controls.Add(this.rugbyJerseyInput);
+            this.Controls.Add(this.rugbyJerseyLabel);
+            this.Controls.Add(this.rugbyCleatsInput);
+            this.Controls.Add(this.rugbyCleatsLabel);
+            this.Controls.Add(this.rugbyBallInput);
+            this.Controls.Add(this.rugbyBallCount);
+            this.Controls.Add(this.reciptLabel);
+            this.Controls.Add(this.shopLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label shopLabel;
+        private System.Windows.Forms.Label reciptLabel;
+        private System.Windows.Forms.Label rugbyBallCount;
+        private System.Windows.Forms.TextBox rugbyBallInput;
+        private System.Windows.Forms.Label rugbyCleatsLabel;
+        private System.Windows.Forms.TextBox rugbyCleatsInput;
+        private System.Windows.Forms.Label rugbyJerseyLabel;
+        private System.Windows.Forms.TextBox rugbyJerseyInput;
+        private System.Windows.Forms.Button calculateButton;
+        private System.Windows.Forms.Label subtotalLabel;
+        private System.Windows.Forms.Label subtotalOutput;
+        private System.Windows.Forms.Label taxLabel;
+        private System.Windows.Forms.Label taxOutput;
+        private System.Windows.Forms.Label totalLabel;
+        private System.Windows.Forms.Label totalOutput;
+        private System.Windows.Forms.Label tenderedLabel;
+        private System.Windows.Forms.TextBox tenderedInput;
+        private System.Windows.Forms.Button changeButton;
+        private System.Windows.Forms.Label changeLabel;
+        private System.Windows.Forms.Label changeOutput;
+        private System.Windows.Forms.Button printButton;
+        private System.Windows.Forms.Label reciptNameLabel;
+        private System.Windows.Forms.Button newOrderButton;
+        private System.Windows.Forms.Button pricesButton;
+    }
+}
+
---
+++ CashRegister/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace CashRegister
+{
+    public partial class Form1 : Form
+    {
+        double rugbyBallPrice = 49.99;
+        double rugbyCleatsPrice = 79.99;
+        double rugbyJerseyPrice = 89.99;
+
+        int rugbyBallPurchased = 0;
+        int rugbyCleatsPurchased = 0;
+        int rugbyJerseyPurchased = 0;
+
+        double rugbyBallTotal = 0;
+        double rugbyCleatsTotal = 0;
+        double rugbyJerseyTotal = 0;
+
+        double subTotal = 0;
+        double taxRate = 0.13;
+        double totalTax = 0;
+        double totalCost = 0;
+
+        int totalTendered = 0;
+        double changeDue = 0;
+
+        // Using int instead of double so that only full numbers can be used
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void calculateButton_Click(object sender, EventArgs e)
+        { // Clear recipt
+            reciptLabel.Text = "";
+            try // Check if rugby ball input is number
+            {
+                rugbyBallPurchased = Convert.ToInt32(rugbyBallInput.Text);
+                
+                if (rugbyBallPurchased < 0)
+                {
+                    rugbyBallInput.Text = "0";
+                    rugbyBallPurchased = 0;
+                }
+            }     
+            catch
+            {
+                rugbyBallInput.Text = "0";
+                rugbyBallPurchased = 0;
+                this.Refresh();
+            }
+
+            try // Check if rugby cleats input is number
+            {
+                rugbyCleatsPurchased = Convert.ToInt32(rugbyCleatsInput.Text);
+
+                if (rugbyCleatsPurchased < 0)
+                {
+                    rugbyCleatsInput.Text = "0";
+                    rugbyCleatsPurchased = 0;
+                }
+            }
+            catch
+            {
+                rugbyCleatsInput.Text = "0";
+                rugbyCleatsPurchased = 0;
+                this.Refresh();
+            }
+
+            try // Check if rugby jersey input is number
+            {
+                rugbyJerseyPurchased = Convert.ToInt32(rugbyJerseyInput.Text);
+
+                if (rugbyJerseyPurchased < 0)
+                {
+                    rugbyJerseyInput.Text = "0";
+                    rugbyJerseyPurchased = 0;
+                }
+            }
+            catch
+            {
+                rugbyJerseyInput.Text = "0";
+                rugbyJerseyPurchased = 0;
+                this.Refresh();
+            }
+
+            // Set a maximum of 9999 purchase
+            if (rugbyBallPurchased > 9999)
+            {
+                rugbyBallPurchased = 9999;
+                rugbyBallInput.Text = "9999";
+                this.Refresh();
+            }
+            if (rugbyCleatsPurchased > 9999)
+            {
+                rugbyCleatsPurchased = 9999;
+                rugbyCleatsInput.Text = "9999";
+                this.Refresh();
+            }
+            if (rugbyJerseyPurchased > 9999)
+            {
+                rugbyJerseyPurchased = 9999;
+                rugbyJerseyInput.Text = "9999";
+                this.Refresh();
+            }
+
+            // Calculation stuff
+            rugbyBallTotal = rugbyBallPrice * rugbyBallPurchased;
+            rugbyCleatsTotal = rugbyCleatsPrice * rugbyCleatsPurchased;
+            rugbyJerseyTotal = rugbyJerseyPrice * rugbyJerseyPurchased;
+
+            subTotal = rugbyBallTotal + rugbyCleatsTotal + rugbyJerseyTotal;
+            totalTax = subTotal * taxRate;
+            totalCost = totalTax + subTotal;
+
+            subtotalOutput.Text = $"{subTotal.ToString("C")}";
+            taxOutput.Text = $"{totalTax.ToString("C")}";
+            totalOutput.Text = $"{totalCost.ToString("C")}";
+
+            changeButton.Enabled = true;
+
+            // Check if all purchases are 0
+            if (rugbyBallPurchased == 0)
+            {
+                if (rugbyCleatsPurchased == 0)
+                {
+                    if (rugbyJerseyPurchased == 0)
+                    {
+                        reciptLabel.Text = "\n\nWhy would you buy nothing?";
+                        changeButton.Enabled = false;
+
+                        subtotalOutput.Text = $"";
+                        taxOutput.Text = $"";
+                        totalOutput.Text = $"";
+                    }
+                }
+            }
+
+
+        }
+
+        private void changeButton_Click(object sender, EventArgs e)
+        { // Clear recipt
+            reciptLabel.Text = "";
+            try // Check to see if tendered input is a number
+            {
+                totalTendered = Convert.ToInt32(tenderedInput.Text);               
+            }
+            catch 
+            {
+                reciptLabel.Text = $"\n\n\n {tenderedInput.Text}";
+                reciptLabel.Text += "\nSomething is wrong with that.";
+                reciptLabel.Text += "\nMake it a whole number or an actual number.";
+
+                tenderedInput.Text = "";
+                this.Refresh();
+
+                totalTendered = 0;
+                printButton.Enabled = false;
+            }
+
+            // Change calculation
+            changeDue = totalTendered - totalCost;
+
+            // Check that user is paying enough
+            if (changeDue < 0)
+            {
+                reciptLabel.Text += "\n\n\nIncrease amount tendered!";
+            }
+            else
+            {
+                changeOutput.Text = $"{changeDue.ToString("C")}";
+                reciptLabel.Text = "";
+
+                printButton.Enabled = true;
+                reciptLabel.Text = "";
+            }
+        }
+
+        private void printButton_Click(object sender, EventArgs e)
+        { // Clears recipt and gets ready to print text
+            reciptLabel.Text = "";
+            reciptNameLabel.Text = "Rugby Shop";
+            Thread.Sleep(1000);
+            this.Refresh();
+            reciptLabel.Text = "\n\n";
+
+            if (rugbyBallPurchased > 0) // Check if rugby balls where purchased
+            {
+                reciptLabel.Text += $"\nRugby Balls X{rugbyBallPurchased} @ {rugbyBallTotal.ToString("C")}";
+                Thread.Sleep(1000);
+                this.Refresh();
+            }
+            
+            if (rugbyCleatsPurchased > 0) // Check if rugby cleats where purchased
+            {
+                reciptLabel.Text += $"\nRugby Cleats X{rugbyCleatsPurchased} @ {rugbyCleatsTotal.ToString("C")}";
+                Thread.Sleep(1000);
+                this.Refresh();
+            }
+            
+            if (rugbyJerseyPurchased > 0) // Check if rugby jerseys where purchased
+            {
+                reciptLabel.Text += $"\nRugby Jerseys X{rugbyJerseyPurchased} @ {rugbyJerseyTotal.ToString("C")}";
+                Thread.Sleep(1000);
+                this.Refresh();
+            }
+            
+            // Print other importat stuff
+            reciptLabel.Text += $"\n\nSubtotal   {subTotal.ToString("C")}";
+            Thread.Sleep(1000);
+            this.Refresh();
+            reciptLabel.Text += $"\nTax   {totalTax.ToString("C")}";
+            Thread.Sleep(1000);
+            this.Refresh();
+            reciptLabel.Text += $"\nTotal   {totalCost.ToString("C")}";
+            Thread.Sleep(1000);
+            this.Refresh();
+
+            reciptLabel.Text += $"\n\nTendered   {totalTendered.ToString("C")}";
+            Thread.Sleep(1000);
+            this.Refresh();
+            reciptLabel.Text += $"\nChange   {changeDue.ToString("C")}";
+            Thread.Sleep(1000);
+            this.Refresh();
+
+        }
+
+        private void newOrderButton_Click(object sender, EventArgs e)
+        { // Resets everything
+            reciptLabel.Text = "";
+            rugbyBallInput.Text = "";
+            rugbyCleatsInput.Text = "";
+            rugbyJerseyInput.Text = "";
+            subtotalOutput.Text = "";
+            taxOutput.Text = "";
+            totalOutput.Text = "";
+            tenderedInput.Text = "";
+            changeOutput.Text = "";
+            reciptLabel.Text = "";
+            reciptNameLabel.Text = "";
+
+            rugbyBallPurchased = 0;
+            rugbyCleatsPurchased = 0;
+            rugbyJerseyPurchased = 0;
+
+            rugbyBallTotal = 0;
+            rugbyCleatsTotal = 0;
+            rugbyJerseyTotal = 0;
+
+            subTotal = 0;
+            totalTax = 0;
+            totalCost = 0;
+
+            totalTendered = 0;
+            changeDue = 0;
+
+            printButton.Enabled = false;
+            changeButton.Enabled = false;
+
+
+
+        }
+
+        private void rugbyBallCount_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pricesButton_Click(object sender, EventArgs e)
+        { // Displays prices
+            reciptNameLabel.Text = "Rugby Shop";
+            reciptLabel.Text = $"\n\n\nRugby Balls - {rugbyBallPrice.ToString("C")}";
+            reciptLabel.Text += $"\nPairs of Rugby Cleats - {rugbyCleatsPrice.ToString("C")}";
+            reciptLabel.Text += $"\nRugby Jerseys - {rugbyJerseyPrice.ToString("C")}";
+        }
+    }
+}
